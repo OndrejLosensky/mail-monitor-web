b4957cbe9923ba0cff2bb2c39091435035bd3b1e
Ondrej Losensky <losenskyondrej@gmail.com>
2024-02-08 19:05:19 +0100

    download from google drive

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import logo from "../assets/logo.png"
 const Body = () => {
+    const handleDownload = () => {
+        const downloadLink = document.createElement('a');
+        downloadLink.href = 'https://drive.google.com/drive/u/0/folders/1B24hzapQYdFirVrDJSqBSKHCtiZuEq3g';
+        downloadLink.download = 'MailMonitor.app';
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+      };
+
   return (
     <div className='w-full h-[600px] flex items-center justify-center'>
     {/* Left side */}
@@ -8,7 +17,7 @@ const Body = () => {
         <div className="text-center">
             <h2 className="text-6xl font-regular pb-6">MailMonitor</h2>
             <p className="text-lg">MailMonitor is Mac menubar application <br></br> for displaying unread emails from your inbox. <br></br> Also featuring auto reading, exporting <br></br>or generating report</p>
-            <button className="border-borderBlack border-2 hover:bg-borderBlack hover:text-TextColor duration-300 text-bordeBlack font-bold py-4 px-12 rounded mt-6">
+            <button onClick={handleDownload} className="border-borderBlack border-2 hover:bg-borderBlack hover:text-TextColor duration-300 text-bordeBlack font-bold py-4 px-12 rounded mt-6">
                 Download
             </button>
         </div>
